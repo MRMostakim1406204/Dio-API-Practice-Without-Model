3c085e8a01ad9b9d7d12b521dfd75d4e13777ea5
MRMostakim1406204 <mdmrmostakim1406204@gmail.com>
2024-05-15 12:52:48 +0600

    Dio Package API Practise Without Model

---
+++ lib/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<List<dynamic>> fetchAPI() async {
+    Dio dio = Dio();
+    var response = await dio.get("https://jsonplaceholder.typicode.com/users");
+    print(response.data.toString());
+    return response.data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+          future: fetchAPI(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${snapshot.data![index]["name"]}",
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Text("${snapshot.data![index]["username"]}",
+                            style:  const TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500),),
+                            Text("${snapshot.data![index]["email"]}"),
+                            Text("${snapshot.data![index]["phone"]}"),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
